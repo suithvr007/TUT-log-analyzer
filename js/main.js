225fa99d4102fdf45cc5a3ddabeb4e3a36080fe1
suithvr007 <42196724+suithvr007@users.noreply.github.com>
2026-01-30 02:50:39 +0530

    Implement log file analysis and rendering features
    Added functionality to analyze log files and render various charts and heatmaps based on the parsed data.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 
+import { parseFile } from "./parser.js";
+import { renderErrorChart } from "./charts.js";
+import { renderTimelineHeatmap } from "./heatmap.js";
+import { renderModuleHeatmap } from "./moduleHeatmap.js";
+import { renderCrashCorrelation } from "./correlation.js";
+import { renderGroupedCrashes } from "./ui.js";
+import { state } from "./state.js";
+
+document.getElementById("analyzeBtn").onclick = () => {
+  Object.assign(state.errorCounts, { Crash:0, ANR:0, StageNow:0 });
+  state.timelineBuckets = {};
+  state.timelineLogs = {};
+  state.moduleHeatmap = {};
+  state.crashCorrelations = [];
+  state.crashGroups = {};
+
+  const files = document.getElementById("logFile").files;
+  let done = 0;
+
+  [...files].forEach(f => {
+    const r = new FileReader();
+    r.onload = e => {
+      parseFile(f, e.target.result);
+      if (++done === files.length) {
+        renderErrorChart();
+        renderTimelineHeatmap();
+        renderModuleHeatmap();
+        renderCrashCorrelation();
+        renderGroupedCrashes();
+      }
+    };
+    r.readAsText(f);
+  });
+};

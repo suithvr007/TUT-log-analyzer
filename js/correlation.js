9e57aebb188e74ba02a8ed1db5b273aed1e829f3
suithvr007 <42196724+suithvr007@users.noreply.github.com>
2026-04-06 00:06:51 +0530

    Delete js directory

--- js/correlation.js
+++
@@ -1,5 +0,0 @@
-import { state } from './state.js';
-
-export function correlateCrashes() {
-  return state.errors.map(e => `Crash at ${e.time} in ${e.module}`);
-}
--- js/state.js
+++
@@ -1,5 +0,0 @@
-export const state = {
-  errors: [],
-  timeline: {},
-  modules: {}
-};
